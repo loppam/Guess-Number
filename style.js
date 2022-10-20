bc0a7a838d1db00783c07ea69a6476de0a1938a4
loppam <lolade132@gmail.com>
2022-10-20 15:35:54 +0100

    Na God run am

--- style.js
+++ style.js
@@ -6,12 +6,17 @@ let highscore = 0;
 const displayMessage = (message) => {
   document.querySelector(".message").textContent = message;
 };
+const displayRemark = (remark) => {
+  document.querySelector(".label-remark").textContent = remark;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   // When there is no input or zero
   if (!guess) {
     displayMessage("⛔️ No Number!");
+    document.querySelector(".score").textContent = 0;
+    displayRemark("You need put number na");
   }
   // When Guess is correct
   else if (guess === number) {
@@ -37,6 +42,34 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+  //Abusive Comments🤭🤭🤭
+  if (score == 20) {
+    displayRemark("😎 Agba Baller");
+  }
+  if (score == 19) {
+    displayRemark("😅 You can do it");
+  }
+  if (score == 18) {
+    displayRemark("😗 Try Harder");
+  }
+  if (score == 17) {
+    displayRemark("🤷🏾 Are you Even Trying?");
+  }
+  if (score == 16) {
+    displayRemark("🧐 You no Know Book?");
+  }
+  if (score == 15) {
+    displayRemark("🤐 Try Thinking‼");
+  }
+  if (score == 14) {
+    displayRemark("😭 You don dey lose oo");
+  }
+  if (score <= 13) {
+    displayRemark("😭 You no Know anyhting!");
+  }
+  if (score <= 9) {
+    displayRemark("😭OLODO😭");
+  }
 });
 
 document.querySelector(".again").addEventListener("click", () => {
@@ -46,6 +79,7 @@ document.querySelector(".again").addEventListener("click", () => {
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
   document.querySelector(".guess").value = "";
+  displayRemark("Hmm...");
   score = 20;
   number = Math.trunc(Math.random() * 20) + 1;
   console.log(number);
